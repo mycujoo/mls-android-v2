bfa4b1dc29c0fba3a88e2ff84039d324c311b283
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-01-04 12:48:35 +0100

    VideoPlayer works now in compose

--- app-compose/src/main/kotlin/tv/mycujoo/annotationisolation/MainComposeActivity.kt
+++ app-compose/src/main/kotlin/tv/mycujoo/annotationisolation/MainComposeActivity.kt
@@ -3,21 +3,104 @@ package tv.mycujoo.annotationisolation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import org.joda.time.DateTime
+import timber.log.Timber
+import tv.mycujoo.mclscore.entity.EventStatus
+import tv.mycujoo.mclscore.model.EventEntity
+import tv.mycujoo.mclscore.model.Stream
+import tv.mycujoo.mclsplayer.compose.MCLSVideo
 
-class MainComposeActivity: ComponentActivity() {
+class MainComposeActivity : ComponentActivity() {
+
+    private val composePlayer = MCLSVideo()
+
+    var event: EventEntity = sampleEvent1()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            Greetings()
+            Column {
+                Text(text = "Number")
+                Button(
+                    onClick = {
+                        Timber.d("Click!!!")
+                        if (event == sampleEvent1()) {
+                            event = sampleEvent2()
+                            composePlayer.playEvent(event)
+                        } else {
+                            event = sampleEvent1()
+                            composePlayer.playEvent(event)
+                        }
+                    }
+                ) {
+                    Text(text = "Click Me")
+                }
+
+                composePlayer.MCLSPlayerView(
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
-}
 
-@Preview
-@Composable
-fun Greetings() {
-    return Text(text = "Hello World")
+    private fun sampleEvent1() = EventEntity(
+        id = "1",
+        description = "DESC",
+        is_test = true,
+        is_protected = false,
+        isNativeMLS = false,
+        organiser = null,
+        metadata = null,
+        location = null,
+        poster_url = null,
+        start_time = DateTime.now(),
+        status = EventStatus.EVENT_STATUS_STARTED,
+        streams = listOf(
+            Stream(
+                id = "5",
+                fullUrl = "https://europe-west-hls.mls.mycujoo.tv/esgp/clb3fuaog0279017240juwas9/master.m3u8",
+                dvrWindowString = null,
+                widevine = null,
+            )
+        ),
+        timeline_ids = listOf(),
+        thumbnailUrl = null,
+        title = "Event",
+        timezone = null,
+    )
+
+    private fun sampleEvent2() = EventEntity(
+        id = "1",
+        description = "DESC",
+        is_test = true,
+        is_protected = false,
+        isNativeMLS = false,
+        organiser = null,
+        metadata = null,
+        location = null,
+        poster_url = null,
+        start_time = DateTime.now(),
+        status = EventStatus.EVENT_STATUS_STARTED,
+        streams = listOf(
+            Stream(
+                id = "5",
+                fullUrl = "https://europe-west-hls.mls.mycujoo.tv/esgp/clb3fuaog0279017240juwas9/master.m3u8",
+                dvrWindowString = null,
+                widevine = null,
+            )
+        ),
+        timeline_ids = listOf(),
+        thumbnailUrl = null,
+        title = "Event",
+        timezone = null,
+    )
 }
---
+++ mcls-player-compose/src/main/kotlin/tv/mycujoo/mclsplayer/compose/MCLSVideo.kt
@@ -0,0 +1,43 @@
+package tv.mycujoo.mclsplayer.compose
+
+import android.view.View
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import tv.mycujoo.mclscore.model.EventEntity
+import tv.mycujoo.mclsplayer.player.MCLSPlayer
+import tv.mycujoo.mclsplayer.player.widget.MCLSPlayerViewImpl
+
+class MCLSVideo {
+
+    var mclsPlayer: MCLSPlayer? = null
+
+    @Composable
+    fun MCLSPlayerView(modifier: Modifier = Modifier) {
+        AndroidView(
+            factory = { context ->
+                val playerView = MCLSPlayerViewImpl(context)
+
+                if (mclsPlayer == null) {
+                    mclsPlayer = MCLSPlayer.Builder()
+                        .withContext(context)
+                        .withPlayerView(playerView)
+                        .build()
+                }
+
+                playerView
+            },
+            modifier = modifier
+        )
+    }
+
+    fun playEvent(event: EventEntity) {
+        mclsPlayer?.playEvent(event)
+    }
+
+    @Composable
+    fun MCLSPlayerViewPreview() {
+        MCLSPlayerView()
+    }
+}
+
