2807427bc0dd64cc6d02bf849bbdea0e963f4b48
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-01-06 15:37:29 +0100

    Cleaning Sample app

--- app/src/main/kotlin/tv/mycujoo/annotationisolation/app/ApplicationClass.kt
+++ app/src/main/kotlin/tv/mycujoo/annotationisolation/app/ApplicationClass.kt
@@ -1,14 +1,11 @@
 package tv.mycujoo.annotationisolation.app
 
 import android.app.Application
-import tv.mycujoo.annotationisolation.di.DaggerApplicationComponent
-import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
 import timber.log.Timber
 import javax.inject.Inject
 
-class ApplicationClass : Application(), HasAndroidInjector {
+class ApplicationClass : Application() {
 
     @Inject
     lateinit var androidInjector : DispatchingAndroidInjector<Any>
@@ -16,15 +13,5 @@ class ApplicationClass : Application(), HasAndroidInjector {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-
-        DaggerApplicationComponent
-            .builder()
-            .bindContext(this)
-            .create()
-            .inject(this)
-    }
-
-    override fun androidInjector(): AndroidInjector<Any> {
-        return androidInjector
     }
 }
