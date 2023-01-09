a1cddd55bbfe2f160dba49d12a3c9067ae04bbc5
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-01-09 12:43:26 +0100

    Added DebugActivity.kt for testing

---
+++ mcls-player/src/debug/kotlin/tv/mycujoo/mclsplayer/player/DebugActivity.kt
@@ -0,0 +1,15 @@
+package tv.mycujoo.mclsplayer.player
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import tv.mycujoo.mclsplayer.databinding.ActivityDebugBinding
+
+class DebugActivity : FragmentActivity() {
+    lateinit var binding: ActivityDebugBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDebugBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
