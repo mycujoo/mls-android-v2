842e0be33175c9b2b70e89e5e9cbd5dc221fefb2
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-02-23 11:37:07 +0100

    Added a sample player base files

---
+++ samples/player/src/main/kotlin/tv/mycujoo/player/PlayerSampleActivity.kt
@@ -0,0 +1,11 @@
+package tv.mycujoo.player
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayerSampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
