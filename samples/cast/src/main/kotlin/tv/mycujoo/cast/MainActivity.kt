699b8f51672abdb4be8c8f4f6a9c6732bece755d
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-02-23 15:20:49 +0100

    Cast now testable in the app

--- samples/cast/src/main/kotlin/tv/mycujoo/cast/MainActivity.kt
+++ samples/cast/src/main/kotlin/tv/mycujoo/cast/MainActivity.kt
@@ -1,16 +1,21 @@
 package tv.mycujoo.cast
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import tv.mycujoo.cast.databinding.ActivityMainBinding
+import tv.mycujoo.mclscast.MCLSCast
+import tv.mycujoo.mclscast.manager.CastApplicationListener
 
 class MainActivity : AppCompatActivity() {
 
     private val vm: MainActivityViewModel by viewModels()
 
+    var castPlayer: MCLSCast? = null
+
     private val adapter = MCLSEventsAdapter {
-        // Play in Connected Device
+        castPlayer?.playEvent(it, true)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +26,28 @@ class MainActivity : AppCompatActivity() {
         vm.buildClient(this)
         vm.getEvents()
         binding.rvEvents.adapter = adapter
+
+        MCLSCast
+            .Builder()
+            .withActivity(this)
+            .withAppId("4381F502")
+            .withMediaButton(binding.mediaRouteButton)
+            .withLifecycle(lifecycle)
+            .withRemotePlayerView(binding.remotePlayerView)
+            .withPublicKey("FBVKACGN37JQC5SFA0OVK8KKSIOP153G")
+            .build {
+                castPlayer = it
+            }
+
+        castPlayer?.addListener(object : CastApplicationListener {
+            override fun onApplicationConnected() {
+                binding.remotePlayerView.visibility = View.VISIBLE
+            }
+
+            override fun onApplicationDisconnected() {
+                binding.remotePlayerView.visibility = View.GONE
+            }
+        })
 
         startListening()
     }
