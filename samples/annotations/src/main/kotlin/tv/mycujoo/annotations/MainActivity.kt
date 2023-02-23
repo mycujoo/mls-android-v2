285f9c8f69e5a02ac3c47a47dc9413e54d426946
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-02-23 11:55:26 +0100

    Added an activity to test ExoPlayer integration with AnnotationsView

---
+++ samples/annotations/src/main/kotlin/tv/mycujoo/annotations/MainActivity.kt
@@ -0,0 +1,14 @@
+package tv.mycujoo.annotations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tv.mycujoo.annotations.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
