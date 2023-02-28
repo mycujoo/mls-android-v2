b10ad6fe93ec2a4bc2a9b9e214dc1abc6c74a324
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-02-28 15:04:32 +0100

    Fixed tests

--- mcls-network/src/test/kotlin/tv/mycujoo/mclsnetwork/factory/EventFactory.kt
+++ mcls-network/src/test/kotlin/tv/mycujoo/mclsnetwork/factory/EventFactory.kt
@@ -1,10 +1,7 @@
 package tv.mycujoo.mclsnetwork.factory
 
 import tv.mycujoo.mclscore.entity.EventStatus
-import tv.mycujoo.mclscore.model.MCLSEvent
-import tv.mycujoo.mclscore.model.Location
-import tv.mycujoo.mclscore.model.Metadata
-import tv.mycujoo.mclscore.model.Stream
+import tv.mycujoo.mclscore.model.*
 import java.util.Calendar
 
 object EventFactory {
@@ -25,6 +22,38 @@ object EventFactory {
         metadata: Metadata? = null,
         is_test: Boolean = true,
     ) = MCLSEvent(
+        id,
+        title,
+        description,
+        thumbnailUrl,
+        poster_url,
+        location,
+        organiser,
+        start_time,
+        status,
+        streams,
+        timezone,
+        timeline_ids,
+        metadata,
+        is_test
+    )
+
+    fun getEventListItem(
+        id: String = "1",
+        title: String = "Event Title",
+        description: String? = null,
+        thumbnailUrl: String? = null,
+        poster_url: String? = null,
+        location: Location? = null,
+        organiser: String? = null,
+        start_time: Calendar? = null,
+        status: EventStatus = EventStatus.EVENT_STATUS_SCHEDULED,
+        streams: List<Stream> = emptyList(),
+        timezone: String? = null,
+        timeline_ids: List<String> = emptyList(),
+        metadata: Metadata? = null,
+        is_test: Boolean = true,
+    ) = MCLSEventListItem(
         id,
         title,
         description,
