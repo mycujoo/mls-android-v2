307d55a035bd5b013516fd833134679c14031d41
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-05-16 11:47:59 +0200

    Added sample code for using MCLS TV SDK

---
+++ samples/tv/src/main/kotlin/tv/mycujoo/tv/MainActivity.kt
@@ -0,0 +1,83 @@
+package tv.mycujoo.tv
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import timber.log.Timber
+import tv.mycujoo.mcls.tv.MCLSTVFragment
+import tv.mycujoo.mclscore.entity.EventStatus
+import tv.mycujoo.mclscore.model.AnnotationAction
+import tv.mycujoo.mclscore.model.MCLSEvent
+import tv.mycujoo.mclscore.model.MCLSStream
+import tv.mycujoo.mclscore.model.PositionGuide
+import tv.mycujoo.mclscore.model.SvgData
+import tv.mycujoo.mclscore.model.ViewSpec
+import tv.mycujoo.tv.databinding.ActivityMainBinding
+
+class MainActivity : FragmentActivity() {
+
+    private val mclsTvFragment = MCLSTVFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Timber.plant(Timber.DebugTree())
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.fragmentContainer.id, mclsTvFragment)
+            .commit()
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+
+        mclsTvFragment.playEvent(
+            MCLSEvent(
+                id = "1",
+                title = "Awesome Title",
+                description = null,
+                thumbnailUrl = null,
+                poster_url = null,
+                location = null,
+                organiser = null,
+                start_time = null,
+                status = EventStatus.EVENT_STATUS_STARTED,
+                streams = listOf(
+                    MCLSStream(
+                        id = "StreamID",
+                        fullUrl = "https://europe-west-hls.mls.mycujoo.tv/esgp/clb3fuaog0279017240juwas9/master.m3u8",
+                        dvrWindowString = null,
+                        widevine = null,
+                    )
+                ),
+                timezone = null,
+                timeline_ids = emptyList(),
+                metadata = null,
+                is_test = true
+            )
+        )
+
+        mclsTvFragment.setAnnotationActions(
+            listOf(
+                AnnotationAction.ShowOverlayAction(
+                    absoluteTime = 0,
+                    offset = 0,
+                    id = "1",
+                    duration = 5000,
+                    svgData = SvgData(
+                        svgUrl = "https://mycujoo-static.imgix.net/eleven_svg_scoreboard.svg"
+                    ),
+                    viewSpec = ViewSpec(
+                        positionGuide = PositionGuide(
+                            left = 0f,
+                            top = 0f
+                        ),
+                        size = Pair(35F, 100F)
+                    )
+                )
+            )
+        )
+    }
+}
