c115ce412fa58219eb989491774fbeb11c2f9b1f
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-05-30 12:37:51 +0200

    Fixed VideoPlayerConfig override

--- mcls-player/src/main/kotlin/tv/mycujoo/mclsplayer/player/mediator/VideoPlayerMediator.kt
+++ mcls-player/src/main/kotlin/tv/mycujoo/mclsplayer/player/mediator/VideoPlayerMediator.kt
@@ -1,8 +1,11 @@
 package tv.mycujoo.mclsplayer.player.mediator
 
 import tv.mycujoo.mclscore.model.MCLSEvent
+import tv.mycujoo.mclsplayercore.config.VideoPlayerConfig
 
 interface VideoPlayerMediator {
+    fun setConfig(config: VideoPlayerConfig)
+
     fun playEvent(event: MCLSEvent)
 
     fun currentPosition(): Long
