1e3cb7e084aae5670397e8f5b19ac740fadbff9e
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-05-31 13:19:06 +0200

    Removed debug activity from Documentation

--- mcls-player/src/debug/kotlin/tv/mycujoo/mclsplayer/player/DebugActivity.kt
+++ mcls-player/src/debug/kotlin/tv/mycujoo/mclsplayer/player/DebugActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import tv.mycujoo.mclsplayer.databinding.ActivityDebugBinding
 
+/**
+ * @suppress
+ */
 class DebugActivity : FragmentActivity() {
     lateinit var binding: ActivityDebugBinding
 
